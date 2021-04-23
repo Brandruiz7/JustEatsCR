598bc0179959aaa0bd9583b8869ac40decb83e9f
Kervin0205 <78397706+Kervin0205@users.noreply.github.com>
2021-04-23 17:04:47 -0600

    Update Cliente.java
    Gente agregue los atributos correspondientes a la clase

--- Proyecto/Just_EatsCR/src/just_eatscr/Cliente.java
+++ Proyecto/Just_EatsCR/src/just_eatscr/Cliente.java
@@ -1,9 +1,9 @@
 /*
  * Cliente
  *
- * @version 1.0
+ * @version 1.01
  *
- * Fecha 17-03-2021
+ * Fecha 23-04-2021
  *
  * Copyright (c) "Preguntar a la profe"
  */
@@ -13,11 +13,18 @@ package just_eatscr;
  * Esta es la clase Cliente, acá podemos encontrar la información sobre
  * el usuario y datos de inicio de Sesión en la app Just Eats.
  *   
- * @author     Brandon Ruiz
- * @version    1.00 17 Mar 2021
+ * @author     Brandon Ruiz, Kervin Ruiz, Christopher Hernandez.
+ * @version    1.01 23 Abr 2021
  * 
 */
 
-public class Cliente {
-    
+public class Cliente 
+{
+  private String ID_Cliente = "";
+  private String Nombre = "";
+  private String Usuario = "";
+  private String Password = "";
+  private String Ubicación = "";
+  private String Correo = "";
+  private String Teléfono = "";  
 }
