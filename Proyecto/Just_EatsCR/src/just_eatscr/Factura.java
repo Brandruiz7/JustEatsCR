82f8092563654397027f03d64a4b2fb4a3be2cb7
Christopher Hernandez <78166548+ChristopherHG-007@users.noreply.github.com>
2021-04-23 20:35:24 -0600

    Update Factura.java

--- Proyecto/Just_EatsCR/src/just_eatscr/Factura.java
+++ Proyecto/Just_EatsCR/src/just_eatscr/Factura.java
@@ -1,9 +1,9 @@
 /*
  * Factura
  *
- * @version 1.04
+ * @version 1.06
  *
- * Fecha 06-04-2021
+ * Fecha 22-04-2021
  *
  * Copyright (c) "Preguntar a la profe"
  */
@@ -17,7 +17,7 @@ import javax.swing.JOptionPane;
  * de Valor Agregado (I.V.A) y el total final.
  *   
  * @author      Brandon Ruiz, Kervin Ruiz, Christopher Hernandez
- * @version     1.04    06 de abril 2021
+ * @version     1.06    22 de abril 2021
  * 
  */
 public class Factura 
@@ -27,18 +27,21 @@ public class Factura
      * también al menos un método que nos ayude con la impresión de la factura 
      */
     
-    private String Codigo_Factura = "";
+    private int Codigo_Factura[] = new int[10];
     private double Total=0;
-    public String Decisión="";
-    public double Saldo=0;
-    public double IVA=0.13;
-    public double CálculoIVA=0;
+    private String Decisión="";
+    private double Saldo=0;
+    private double IVA=0.13;
+    private double CálculoIVA=0;
+    private int Contador=0;
+    
+    Factura Ventas[] = new Factura[10];
 
     /**
      * Este sería el constructor vacío de esta clase.
      */
     
-    public Factura ()
+    public Factura()
     {
         
     }
@@ -51,16 +54,38 @@ public class Factura
     public Factura (double Saldo)
     {
         this.Saldo=Saldo; // Y acá el precio
-    }      
+    }
 
     /**
-     * Este sería el constructor vacío de esta clase.
+     * Este es el metodo encargado de calcular el impuesto de Venta.
      */
     
-    public void Cálculo ()
-    {      
+    public void Cálculo () {      
         CálculoIVA=Saldo*IVA;
         Total=Saldo+CálculoIVA;
+    }
+    
+    /**
+     * En este método, se imprime los datos totales como el sub total, el costo de Iva y el total.
+     */
+    
+    public void metodoPago() {
+        int Option=0;
+        
+        do {
+            Option = Integer.parseInt(JOptionPane.showInputDialog("Metdo de cancelacion."
+                    + "\n1. Tarjeta."
+                    + "\n2. Efectivo"));
+            
+            if (Option == 1) {
+                Decisión = ("Tarjeta");
+                Option = 0;
+            }else if(Option == 2) {
+                Decisión = ("Efectivo");
+                Option = 0;
+            } 
+            
+        }while(Option!=0);
         
     }
     
@@ -68,31 +93,26 @@ public class Factura
      * En este método, se imprime los datos totales como el sub total, el costo de Iva y el total.
      */
     
-    public void Factura ()
-    {
-        Cálculo();
+    public void Facturacion () {
         
         JOptionPane.showMessageDialog(null, "            Just Eats CR              "
-                                         +"\nResumen del pedido:"
-                                         +"\nTotal:"+Saldo
-                                         +"\nImpuesto del Valor Agregado (IVA)"+CálculoIVA
-                                         +"\nEl total de compra realizada es: "+Total
-                                         +"\nEl pedido solicitado llegará en breve."
-                                         +"\n          Gracias por preferirnos");
+                                         + "\nResumen del pedido:"
+                                         + "\nN° Factura: " + Contador
+                                         + "\nTotal: ¢"+Saldo
+                                         + "\nImpuesto del Valor Agregado (IVA): ¢"+CálculoIVA
+                                         + "\nEl total de compra realizada es: ¢"+Total
+                                         + "\nPaga con: " +Decisión
+                                         + "\nEl pedido solicitado llegará en breve."
+                                         + "\n          Gracias por preferirnos"
+                                          + "");
+        Contador++;
+        
     }
-     
     
+    /** 
+     * Getters y setters necesarios para la clase.
+     */
     
-    public String getCodigo_Factura() 
-    {
-        return Codigo_Factura;
-    } 
-    
-    public void setCodigo_Factura(String Codigo_Factura) 
-    {
-        this.Codigo_Factura = Codigo_Factura;
-    }
-
     public double getTotal() 
     {
             
@@ -103,4 +123,44 @@ public class Factura
     {
         this.Total = Total;
     }
+
+    public int[] getCodigo_Factura() {
+        return Codigo_Factura;
+    }
+
+    public void setCodigo_Factura(int[] Codigo_Factura) {
+        this.Codigo_Factura = Codigo_Factura;
+    }
+
+    public String getDecisión() {
+        return Decisión;
+    }
+
+    public void setDecisión(String Decisión) {
+        this.Decisión = Decisión;
+    }
+
+    public double getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(double Saldo) {
+        this.Saldo = Saldo;
+    }
+
+    public double getIVA() {
+        return IVA;
+    }
+
+    public void setIVA(double IVA) {
+        this.IVA = IVA;
+    }
+
+    public double getCálculoIVA() {
+        return CálculoIVA;
+    }
+
+    public void setCálculoIVA(double CálculoIVA) {
+        this.CálculoIVA = CálculoIVA;
+    }    
 }
