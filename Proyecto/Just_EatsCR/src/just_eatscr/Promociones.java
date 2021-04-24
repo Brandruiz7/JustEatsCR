ea9b6686a4bc000c4f6b10fd6e35176c5d43d9dc
Christopher Hernandez <78166548+ChristopherHG-007@users.noreply.github.com>
2021-04-23 19:50:55 -0600

    Update Promociones.java
    Cambios de la versión que se borró

--- Proyecto/Just_EatsCR/src/just_eatscr/Promociones.java
+++ Proyecto/Just_EatsCR/src/just_eatscr/Promociones.java
@@ -1,13 +1,15 @@
 /*
  * Promociones
  *
- * @version 1.01
+ * @version 1.02
  *
- * Fecha 17-03-2021
+ * Fecha 03-04-2021
  *
  * Copyright (c) "Preguntar a la profe"
  */
 package just_eatscr;
+
+import javax.swing.JOptionPane;
 
 /**
  * Esta es la clase Promociones, según la cantidad que compre el cliente,
@@ -23,4 +25,38 @@ public class Promociones
 {
     public String Tipo_Promo = "";
     public double Descuento_Promo; 
+    public double Compra;
+    
+   public Promociones ( String Tipo_Promo, double Descuento_Promo, double Compra)
+    {
+        this.Tipo_Promo=Tipo_Promo;
+        this.Descuento_Promo=Descuento_Promo;
+        this.Compra=Compra;
+        
+    } 
+    
+ 
+    /**
+     * En este método 
+     * se le aplicará un descuento de 15% al cliente, si la compra que ha realizado es mayor 
+     * a ¢10.000 
+     * 
+     */
+    
+ public void Descuentos()
+ {
+   double Compra = Double.parseDouble(JOptionPane.showInputDialog("digite el monto a pagar"));
+
+  if(Compra > 10000)
+  {
+     Compra -=(Compra*0.15); 
+  }  
+  else
+   {
+     Compra=Descuento_Promo;
+   }
+   
+  JOptionPane.showMessageDialog(null," El monto a pagar con el descuento aplicado es: "+ Compra);
+ }
+
 }
